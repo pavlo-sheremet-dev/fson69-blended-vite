040ac56114d773df9820517c8582cc43690b5fde
Denys Volodko <babyleejane@gmail.com>
2023-06-04 16:02:46 +0300

    likes and dislikes

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,25 +1,33 @@
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Button.module.css';
+import { useUpdateCommentMutation } from '../../redux/commentApi';
 
 export const Button = ({ children, counter, role = 'thumbsUp', id }) => {
   const variants = {
     [styles.thumbsUp]: role === 'thumbsUp',
     [styles.thumbsDown]: role === 'thumbsDown',
   };
+  const [updateComment, { isLoading }] = useUpdateCommentMutation();
 
-  const onBtnHandleClick = () => {
-    console.log('click');
+  const [isClicked, setIsClicked] = useState(false);
+
+  const onBtnHandleClick = async () => {
+    const updatedData = { id, [role]: counter + (!isClicked ? 1 : -1) };
+    await updateComment(updatedData);
+    setIsClicked((prev) => !prev);
+    console.log(updatedData);
   };
 
   return (
     <button
       className={classNames(styles.button, variants)}
-      type='button'
+      type="button"
       counter={counter}
       onClick={onBtnHandleClick}
       id={id}
+      disabled={isLoading}
     >
       {children}
 
--- src/redux/commentApi.js
+++ src/redux/commentApi.js
@@ -1,29 +1,39 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const API_ENDPOINT = "/comments";
-const BASE_URL = "https://6393b5f2ab513e12c514f63c.mockapi.io/api";
+const API_ENDPOINT = '/comments';
+const BASE_URL = 'https://6393b5f2ab513e12c514f63c.mockapi.io/api';
 
 export const commentApi = createApi({
-  reducerPath: "comments",
+  reducerPath: 'comments',
   baseQuery: fetchBaseQuery({
     baseUrl: BASE_URL,
   }),
-  tagTypes: ["Comments"],
+  tagTypes: ['Comments'],
   endpoints: (builder) => ({
     // The `getPosts` endpoint is a "query" operation that returns data
     getComments: builder.query({
       // The URL for the request is '/fakeApi/posts'
       query: () => API_ENDPOINT,
+      providesTags: ['Comments'],
     }),
     addNewComment: builder.mutation({
       query: (newComment) => ({
         url: API_ENDPOINT,
-        method: "POST",
+        method: 'POST',
         // Include the entire post object as the body of the request
         body: newComment,
       }),
+      invalidatesTags: ['Comments'],
+    }),
+    updateComment: builder.mutation({
+      query: ({ id, ...comment }) => ({
+        url: `${API_ENDPOINT}/${id}`,
+        method: 'PUT',
+        body: comment,
+      }),
+      invalidatesTags: ['Comments'],
     }),
   }),
 });
 
-export const { useGetCommentsQuery, useAddNewCommentMutation } = commentApi;
+export const { useGetCommentsQuery, useAddNewCommentMutation, useUpdateCommentMutation } = commentApi;
